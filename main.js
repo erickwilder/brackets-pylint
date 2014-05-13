14829b47306e0e186c70e662fc72cb91546e8d2a
Erick Wilder <erickwilder@gmail.com>
2014-05-13 03:44:40 -0300

    Changed to use NodeDomain event structure (still broken)

--- main.js
+++ main.js
@@ -1,34 +1,29 @@
 /*jslint vars: true, nomen: true, indent: 4*/
-/*global define, brackets, console*/
+/*global define, brackets, console, $*/
 define(function (require, exports, module) {
     "use strict";
     
     var AppInit = brackets.getModule('utils/AppInit'),
         CodeInspection = brackets.getModule('language/CodeInspection'),
         ExtensionUtils = brackets.getModule('utils/ExtensionUtils'),
-        DocumentManager = brackets.getModule('document/DocumentManager'),
-        FileSystem = brackets.getModule('filesystem/FileSystem'),
-        NodeDomain = brackets.getModule('utils/NodeDomain'),
-        ProjectManager = brackets.getModule('project/ProjectManager');
+        NodeDomain = brackets.getModule('utils/NodeDomain');
     
-    var PYLINTRC_FILENAME = 'pylintrc';
-    var pylintDomain = new NodeDomain('pylint', ExtensionUtils.getModulePath(module, 'node/Pylint'));
+    var NAMESPACE = 'pylinterDomain';
     
-    function displayResults(output) {
-        console.log("Pylint result = " + output);
-    }
-    
-    function pylinter(text, filePath) {
-        pylintDomain.exec('runPylint', filePath).always(displayResults);
-    }
-    
-    /*TODO: Add mutiple rcfiles lookup*/
-    function _locateConfig() {
-        return ProjectManager.getProjectRoot() + PYLINTRC_FILENAME;
-    }
-
-    CodeInspection.register('python', {
-        "name": "Pylinter",
-        scanFile: pylinter
+    AppInit.appReady(function () {
+        var pylintDomain = new NodeDomain(NAMESPACE, ExtensionUtils.getModulePath(module, 'node/Pylint'));
+        
+        $(pylintDomain).on(NAMESPACE + '.linted', function (event, output) {
+            console.log("Pylint result = " + output);
+        });
+        
+        function pylinter(text, filePath) {
+            pylintDomain.exec('runPylint', filePath);
+        }
+        
+        CodeInspection.register('python', {
+            "name": "Pylinter",
+            scanFile: pylinter
+        });
     });
 });
