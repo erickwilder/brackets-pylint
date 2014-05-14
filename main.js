402a491b9cb047143fa50e160ff98b3a222f47e0
Erick Wilder <erickwilder@gmail.com>
2014-05-14 02:58:30 -0300

    Change pylint call and add parsing

--- main.js
+++ main.js
@@ -3,17 +3,11 @@
 define(function (require, exports, module) {
     "use strict";
     
-    var _ = require('thirdparty/lodash'),
-        CodeInspection = brackets.getModule('language/CodeInspection'),
+    var CodeInspection = brackets.getModule('language/CodeInspection'),
         ExtensionUtils = brackets.getModule('utils/ExtensionUtils'),
         NodeDomain = brackets.getModule('utils/NodeDomain');
     
-    var pylintDomain = new NodeDomain('bracketsPylint', ExtensionUtils.getModulePath(module, 'node/PylintDomain')),
-        pylintTypes = {
-            error: CodeInspection.Type.ERROR,
-            warning: CodeInspection.Type.WARNING,
-            meta: CodeInspection.Type.META
-        };
+    var pylintDomain = new NodeDomain('bracketsPylint', ExtensionUtils.getModulePath(module, 'node/PylintDomain'));
     
     function handleLinterAsync(text, filePath) {
         var deferred = $.Deferred();
@@ -22,9 +16,9 @@ define(function (require, exports, module) {
                 var result = { errors: [] };
                 pylintResult.forEach(function (lintData) {
                     result.errors.push({
-                        pos: {line: lintData.line, ch: lintData.character},
-                        message: lintData.message,
-                        type: pylintTypes[lintData.type]
+                        pos: {line: lintData.line, ch: lintData.col},
+                        message: lintData.msg,
+                        type: lintData.cat
                     });
                 });
                 deferred.resolve(result);
