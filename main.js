4616de282489108a4d30bac834e12ace0c68ba6b
Erick Wilder <erickwilder@gmail.com>
2014-05-14 01:38:53 -0300

    Refactor main extension file to use scanFileAsync

--- main.js
+++ main.js
@@ -3,27 +3,38 @@
 define(function (require, exports, module) {
     "use strict";
     
-    var AppInit = brackets.getModule('utils/AppInit'),
+    var _ = require('thirdparty/lodash'),
         CodeInspection = brackets.getModule('language/CodeInspection'),
         ExtensionUtils = brackets.getModule('utils/ExtensionUtils'),
         NodeDomain = brackets.getModule('utils/NodeDomain');
     
-    var NAMESPACE = 'pylinterDomain';
+    var pylintDomain = new NodeDomain('bracketsPylint', ExtensionUtils.getModulePath(module, 'node/PylintDomain')),
+        pylintTypes = {
+            error: CodeInspection.Type.ERROR,
+            warning: CodeInspection.Type.WARNING,
+            meta: CodeInspection.Type.META
+        };
     
-    AppInit.appReady(function () {
-        var pylintDomain = new NodeDomain(NAMESPACE, ExtensionUtils.getModulePath(module, 'node/Pylint'));
-        
-        $(pylintDomain).on(NAMESPACE + '.linted', function (event, output) {
-            console.log("Pylint result = " + output);
-        });
-        
-        function pylinter(text, filePath) {
-            pylintDomain.exec('runPylint', filePath);
-        }
-        
-        CodeInspection.register('python', {
-            "name": "Pylinter",
-            scanFile: pylinter
-        });
+    function handleLinterAsync(text, filePath) {
+        var deferred = $.Deferred();
+        pylintDomain.exec('run', filePath)
+            .done(function (pylintResult) {
+                var result = { errors: [] };
+                pylintResult.forEach(function (lintData) {
+                    result.errors.push({
+                        pos: {line: lintData.line, ch: lintData.character},
+                        message: lintData.message,
+                        type: pylintTypes[lintData.type]
+                    });
+                });
+                deferred.resolve(result);
+            });
+        return deferred.promise();
+    }
+    
+    // Register a new async linter to `python` language
+    CodeInspection.register('python', {
+        name: 'BracketsPylint',
+        scanFileAsync: handleLinterAsync
     });
 });
